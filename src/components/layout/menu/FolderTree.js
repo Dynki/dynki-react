7f43a652ab37ae44e1091d0fc43a68849da7f823
Dean Selvey <deanselvey@gmail.com>
2019-06-20 22:06:21 +0100

    Changed menu colours and default expanded status

---
+++ src/components/layout/menu/FolderTree.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
+import { Menu, Icon, Drawer } from 'antd';
+import { Droppable } from 'react-beautiful-dnd';
+
+import FolderForm from './FolderForm';
+
+const SubMenu = Menu.SubMenu;
+
+const FolderMenu =  (props) => {
+
+    const [selected, setSelected] = useState(false);
+    const [hovering, setHovering] = useState(false);
+    const [visible, setVisible] = useState(false);
+ 
+    const mouseEnter = () => {
+        setHovering(true);
+    }
+
+    const mouseLeave = () => {
+        setHovering(false);
+    }
+
+    const {handleClick, item} = props;
+
+    return (
+    <React.Fragment>
+        <Drawer
+            title="Edit Folder"
+            placement="right"
+            closable={true}
+            onClose={() => setVisible(false)}
+            visible={visible}
+            width={370}
+        >
+            <FolderForm folder={item} onRemoveFolder={() => setVisible(false)}/>
+        </Drawer>
+        <Droppable droppableId="Folder-Menu" type="menuitems">
+        {provided => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+                <Menu 
+                    className="foldermenu" 
+                    mode="inline"
+                    onMouseEnter={mouseEnter}
+                    onMouseLeave={mouseLeave}
+                >
+                    <SubMenu 
+                        className="folder-subfolder"
+                        onTitleClick={() => setSelected(!selected)}
+                        title={
+                            <span
+                            >
+                                <Icon type={selected ? 'folder-open' : 'folder'} />
+                                <span>{item.title}</span>
+                        </span>
+                        }
+                    >
+                        {item.items ? item.items.map(i2 => (
+                            <Menu.Item key={i2.id}>
+                                <Link onClick={(e) => handleClick(e, i2.action)} to={i2.target} key={i2.id}>
+                                    {i2.title}
+                                </Link>
+                            </Menu.Item>
+                        )) : null}
+                    </SubMenu>
+                    {hovering ?
+                        <React.Fragment>
+                            <Icon type="edit" className="edit" onClick={() => setVisible(true)}/>
+                        </React.Fragment>
+                    :
+                        null
+                    }
+                </Menu>
+            </div>
+        )}
+        </Droppable>
+    </React.Fragment>);
+}
+
+export default FolderMenu;
--- src/components/layout/menu/SubMenuItem.js
+++ src/components/layout/menu/SubMenuItem.js
@@ -10,21 +10,13 @@ const { TreeNode, DirectoryTree } = Tree;
 class DynSubMenu extends React.Component {
 
     onSelect = (keys, event) => {
-        console.log('Trigger Select', keys, event);
-    };
-        
-    // Triggered by clicking a menu item.
-    handleClick = (e, action) => {
         // Dispatches the action associated with this menu item's properties.
-        console.log(action);
-        // this.props.itemClicked(action);
-    }
-
-    // Triggered by clicking a menu items function button (e.g. the add new board button).
-    handleBtnClick = (e, action) => {
-        // Dispatches the action associated with this menu item's properties.
-        console.log(action);
         // this.props.btnClicked(action);
+        const item = this.props.items.find(i => i.id === keys[0]);
+
+        if (item) {
+            this.props.itemClicked(item.action);
+        }
     }
 
     // Triggered by clicking the add new folder button.
@@ -42,56 +34,60 @@ class DynSubMenu extends React.Component {
     }
 
     render() {
-        const {itemClicked, btnClicked, addFolder, title, icon , items, act, selectedKeys, ...other} = this.props;
+        const {itemClicked, btnClicked, addFolder, title, icon , items, act, selectedKeys, currentBoard, ...other} = this.props;
 
         return (
             <div>
             { items ? (
                 <div className="submenu">
-                    {/* <SubMenu {...other} key={title} title={<span><Icon type={icon} /><span>{title}</span></span>}> */}
-                        <DirectoryTree onSelect={this.onSelect} switcherIcon={<Icon type="down" />} draggable>
-                            <TreeNode title="Boards" key={'000001'} icon={<Icon type="schedule" />}>
+                    <DirectoryTree 
+                        onSelect={this.onSelect} 
+                        switcherIcon={<Icon type="down" />} 
+                        draggable 
+                        defaultExpandedKeys={['000001']} 
+                        // defaultSelectedKeys={items ? [items[0].id] : []}
+                    >
+                        <TreeNode title="Boards" key={'000001'} icon={<Icon type="schedule" />}>
 
-                                    { items.map((i, idx) => {
+                                { items.map((i, idx) => {
 
-                                        return i.isFolder ?
-                                                <TreeNode title={i.title} key={i.id}>
-                                                    {i.items ? i.items.map((i2, idx2) => (
-                                                        <TreeNode title={i2.title} key={i2.id} isLeaf />
-                                                    )) : null}
-                                                </TreeNode>
-                                            :
-                                                <TreeNode title={i.title} key={i.id} icon={<div className="noicon"></div>}/>
-                                        }) 
-                
-                                    // <React.Fragment key={idx}>
-                                    //     {i.isFolder ? 
-                                    //     <Draggable key={idx} draggableId={i.id} index={idx}>
-                                    //     {provided => (
-                                    //         <div ref={provided.innerRef} {...provided.draggableProps}  {...provided.dragHandleProps} >
-                                    //             <FolderMenu handleClick={this.handleClick} item={i}/>
-                                    //         </div>
-                                    //     )}
-                                    //     </Draggable>
-                                    //     :
-                                    //     <Draggable key={idx} draggableId={i.id} index={idx}>
-                                    //     {provided => (
-                                    //         <div ref={provided.innerRef} {...provided.draggableProps}  {...provided.dragHandleProps} >
-                                    //             <Menu.Item {...other} key={i.id}>
-                                    //                 <Link onClick={(e) => this.handleClick(e, i.action)} to={i.target} key={i.id}>
-                                    //                     {i.title}
-                                    //                 </Link>
-                                    //             </Menu.Item>
-                                    //             </div>
-                                    //     )}
-                                    //     </Draggable>
-                                    //     }
-                                    // </React.Fragment>
-                                }
-                            </TreeNode>
-                            <Menu.Item {...other} key={'newFolder'} onClick={this.addFolder}><Icon type="folder-add"/> New folder</Menu.Item>
-                        </DirectoryTree>
-                    {/* </SubMenu> */}
+                                    return i.isFolder ?
+                                            <TreeNode title={i.title} key={i.id}>
+                                                {i.items ? i.items.map((i2, idx2) => (
+                                                    <TreeNode title={i2.title} key={i2.id} isLeaf />
+                                                )) : null}
+                                            </TreeNode>
+                                        :
+                                            <TreeNode title={i.title} key={i.id} icon={<div className="noicon"></div>}/>
+                                    }) 
+            
+                                // <React.Fragment key={idx}>
+                                //     {i.isFolder ? 
+                                //     <Draggable key={idx} draggableId={i.id} index={idx}>
+                                //     {provided => (
+                                //         <div ref={provided.innerRef} {...provided.draggableProps}  {...provided.dragHandleProps} >
+                                //             <FolderMenu handleClick={this.handleClick} item={i}/>
+                                //         </div>
+                                //     )}
+                                //     </Draggable>
+                                //     :
+                                //     <Draggable key={idx} draggableId={i.id} index={idx}>
+                                //     {provided => (
+                                //         <div ref={provided.innerRef} {...provided.draggableProps}  {...provided.dragHandleProps} >
+                                //             <Menu.Item {...other} key={i.id}>
+                                //                 <Link onClick={(e) => this.handleClick(e, i.action)} to={i.target} key={i.id}>
+                                //                     {i.title}
+                                //                 </Link>
+                                //             </Menu.Item>
+                                //             </div>
+                                //     )}
+                                //     </Draggable>
+                                //     }
+                                // </React.Fragment>
+                            }
+                        </TreeNode>
+                        <Menu.Item {...other} key={'newFolder'} onClick={this.addFolder}><Icon type="folder-add"/> New folder</Menu.Item>
+                    </DirectoryTree>
                     <Tooltip placement="right" title="New Board">
                         <Button id={'btn' + title} className="btn-add-board" onClick={(e) => this.handleBtnClick(e, act)}  type="dashed" shape="circle" icon="plus"></Button>
                     </Tooltip>
@@ -114,4 +110,10 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(null, mapDispatchToProps)(DynSubMenu);
+const mapStateToProps = (state) => {
+    return {
+        currentBoard: state.boards.currentBoard
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DynSubMenu);
