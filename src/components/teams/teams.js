d07c8e8bb2bd5d59bd09b945cebf9ee9164e495b
Dean Selvey <deanselvey@gmail.com>
2019-09-15 20:41:59 +0100

    Team header added

--- src/components/teams/teams.js
+++ src/components/teams/teams.js
@@ -1,29 +1,172 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { PageHeader, Menu, Dropdown, Icon, Button, Tag, Typography, Row } from 'antd';
+
+const { Paragraph } = Typography;
+
 // Container for teams components.
 export class Teams extends React.Component {
 
+
+
     render() {
-        return (
-            <section className="teams">
-                Yay team
-            </section> 
-        )
-    }
+        const menu = (
+            <Menu>
+                <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                        Disable team
+                </a>
+                </Menu.Item>
+                <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                        Remove team
+                </a>
+                </Menu.Item>
+            </Menu>
+        );
+    
+        const IconLink = ({ src, text }) => (
+            <a
+                style={{
+                    marginRight: 16,
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+            >
+                <img
+                    style={{
+                        marginRight: 8,
+                    }}
+                    src={src}
+                    alt="start"
+                />
+                {text}
+            </a>
+        );
+
+        const content = (
+    
+            <div className="content">
+                <Paragraph>
+                    Below is all the functionality you require to be able to manage your team, go ahead and 
+                    add some users to get started.
+              </Paragraph>
+                <Paragraph>
+                    Why not start by organising your team into groups. For example you could add groups of admin, users
+                    to get going. You can always add more groups and reorganise users between groups at a later point.
+              </Paragraph>
+                <Row className="contentLink" type="flex">
+                    <IconLink
+                        src="https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg"
+                        text="Invite a user"
+                    />
+                    <IconLink
+                        src="https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg"
+                        text=" Disable Team"
+                    />
+                    <IconLink
+                        src="https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg"
+                        text="Team Info"
+                    />
+                </Row>
+            </div>
+        );
+    
+        const DropdownMenu = () => {
+            return (
+                <Dropdown key="more" overlay={menu}>
+                    <Button
+                        style={{
+                            border: 'none',
+                            padding: 0,
+                        }}
+                    >
+                        <Icon
+                            type="ellipsis"
+                            style={{
+                                fontSize: 20,
+                                verticalAlign: 'top',
+                            }}
+                        />
+                    </Button>
+                </Dropdown>
+            );
+        };
+    
+        const routes = [
+            {
+                path: 'team',
+                breadcrumbName: 'Team',
+            },
+            {
+                path: 'first',
+                breadcrumbName: 'Team11',
+            }
+        ];
+    
+        const Content = ({ children, extraContent }) => {
+            return (
+                <Row className="content" type="flex">
+                    <div className="main" style={{ flex: 1 }}>
+                        {children}
+                    </div>
+                    <div
+                        className="extra"
+                        style={{
+                            marginLeft: 80,
+                        }}
+                    >
+                        {extraContent}
+                    </div>
+                </Row>
+            );
+        };
+
+
+        return <PageHeader
+            title="Team11"
+            subTitle="Like spokes in a wheel"
+            tags={<Tag color="blue">Team Enabled</Tag>}
+            extra={[
+                <Button key="3">Add a group</Button>,
+                <Button key="1" type="primary">
+                    Add a team member
+                </Button>,
+                <DropdownMenu key="more" />,
+            ]}
+            avatar={{ src: 'https://avatars1.githubusercontent.com/u/8186664?s=460&v=4' }}
+            breadcrumb={{ routes }}
+        >
+            <Content
+                extraContent={
+                    <img
+                        src="https://gw.alipayobjects.com/mdn/mpaas_user/afts/img/A*KsfVQbuLRlYAAAAAAAAAAABjAQAAAQ/original"
+                        alt="content"
+                    />
+                }
+            >
+                {content}
+            </Content>
+        </PageHeader>
+    };
+
 }
 
 export const mapStateToProps = (state) => {
-    return{
-      teams: state.teams.teams,
-      team: state.teams.currentTeam,
-      progress: state.base.progress
+    return {
+        teams: state.teams.teams,
+        team: state.teams.currentTeam,
+        progress: state.base.progress
     }
 }
 
 export const mapDispatchToProps = (dispatch) => {
-    return{
+    return {
     }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Teams);
+
+
+
