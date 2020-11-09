9ea84e47842a2b81c1f2df5d902a9ff9e750f310
Dean Selvey <deanselvey@gmail.com>
2020-11-09 21:27:05 +0000

    Fixed issue wth popconfirm on dropdown

--- src/components/boards/BoardColumnMenu.js
+++ src/components/boards/BoardColumnMenu.js
@@ -17,49 +17,59 @@ const MenuIcon = styles(EllipsisOutlined)`
     -ms-transform: rotate(90deg);
     -o-transform: rotate(90deg);
     transform: rotate(90deg);
-
 `
 
 const BoardColumnMenu = ({ allowWrite, column, columns, hasRole, onRemoveColumn, user }) => {
 
-    const [hover, setHover] = React.useState("false")
+  const [visible, setVisible] = React.useState(false)
 
-    const handleClick = object => {
-        console.log('click ', object.key)
-    };
+  const handleClick = e => {
+    if (e.key !== '2') {
+      setVisible(false)
+    }
+  }
 
-    const renderMenu = () =>  {
-        if (!user) {
-            return null
-        }
+  const handleVisibleChange = flag => {
+    setVisible(flag)
+  };
 
-        return (
-            <Menu
-                onClick={handleClick}
-            >
-                <Menu.Item disabled={!hasRole('BOARD_CREATORS')}>
-                    <ReorderDrawer hovering={hover} columns={columns} disabled={!hasRole('BOARD_CREATORS')}/>
-                </Menu.Item>
-                <Menu.Item disabled={!hasRole('BOARD_CREATORS')}>
-                    <Popconfirm title="Are you sure delete this column?"
-                        okText="Yes"
-                        cancelText="No Way"
-                        trigger="click"
-                        placement="bottomLeft"
-                        onConfirm={() => onRemoveColumn(column.model)}
-                    >
-                        <a href="no-ref"><EllipsisOutlined style={{ paddingRight: '10px' }} /> Delete Column</a>
-                    </Popconfirm>
-                </Menu.Item>
-            </Menu>
-        )
+  const renderMenu = () => {
+    if (!user) {
+      return null
     }
 
     return (
-        <Dropdown overlay={renderMenu()} placement="bottomRight" onMouseEnter={() => setHover("true")} onMouseLeave={() => setHover("false")}>
-            <MenuIcon type="ellipsis" hovering={hover}/>
-        </Dropdown>
+      <Menu
+        onClick={handleClick}
+      >
+        <Menu.Item disabled={!hasRole('BOARD_CREATORS')} key="1">
+          <ReorderDrawer columns={columns} disabled={!hasRole('BOARD_CREATORS')} />
+        </Menu.Item>
+        <Menu.Item disabled={!hasRole('BOARD_CREATORS')} key="2">
+          <Popconfirm title="Are you sure delete this column?"
+            okText="Yes"
+            cancelText="No Way"
+            trigger="click"
+            placement="bottomLeft"
+            onConfirm={() => onRemoveColumn(column.model)}
+          >
+            <a href="no-ref"><EllipsisOutlined style={{ paddingRight: '10px' }} /> Delete Column</a>
+          </Popconfirm>
+        </Menu.Item>
+      </Menu>
     )
+  }
+
+  return (
+    <Dropdown 
+      overlay={renderMenu()} 
+      placement="bottomRight"
+      onVisibleChange={handleVisibleChange}
+      visible={visible}
+    >
+      <MenuIcon type="ellipsis"/>
+    </Dropdown>
+  )
 }
 
 export default authWrapper(BoardColumnMenu)
